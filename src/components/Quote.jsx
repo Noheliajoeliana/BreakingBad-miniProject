3b05ff560e2f6d4ea2a3b8f468fcf38e359d2d7f
Nohelia <nohelia.j.r.a@gmail.com>
2021-10-21 18:24:04 -0400

    Intrucciones listas

--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -1,9 +1,12 @@
 import React from "react"
 
-export default function Quote() {
+export default function Quote({frase}) {
+    //Quote recibe frase desde las props, prueba cómo le llega esa frase para ver cómo podrías renderizarlo
+    
     return (
         <div>
-            Breaking Bad Quote
+            <h1>Frase</h1>
+            <p>-Autor</p>
         </div>
     )
 }
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,18 +1,31 @@
-import React from "react"
+import React, {useState} from "react"
 import Quote from "../components/Quote"
 
 export default function App() {
+    //Aquí pueden modificar el estado, inicialmente está seteado con un objeto que tiene los atributos quote y author, que son renderizados en el componente <Quote/>
+    const [frase,setFrase] = useState({
+        quote: 'Quote',
+        author: 'author'
+    })
 
     function APIcall(){
         fetch('https://breaking-bad-quotes.herokuapp.com/v1/quotes')
         .then(res => res.json())
-        .then(json => console.log(json[0]))
+        .then(data => {
+            //Al hacer la consulta a la api, le hice console.log al resultado para que puedan ver con qué estan trabajando. Modifíquenlo para hacer algo con ese data[0] 
+            console.log(data[0])
+        })
     }
-
     return (
         <div>
             Hello World
-            <Quote/>
+            <Quote frase={frase} /*Le paso la frase completa a Quote*//>
+
+            
+            {/* La idea es que este botón genere la nueva frase, ¿qué función debería llamarse en su onClick? */}
+            <button>Click me</button>
         </div>
     )
 }
+
+
